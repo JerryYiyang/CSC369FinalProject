22a4fe3603f85f5e3025c9bf758686d9903c2be8
lamkin1 <jamesclamkin@gmail.com>
2024-05-28 14:37:50 -0700

    merged main

--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -7,11 +7,26 @@ import org.apache.log4j.Level
 import scala.collection._
 
 object App {
+  import scala.io.Source
   def main(args: Array[String]):Unit = {
     Logger.getLogger("org").setLevel(Level.OFF)
     Logger.getLogger("akka").setLevel(Level.OFF)
     val conf = new SparkConf().setAppName("NameOfApp").setMaster("local[4]")
     val sc = new SparkContext(conf)
+    println("Hello World!");    //; is optional
+
+    val filePath = "AllCombined.txt"
+
+    // Read the file contents as a string
+    val fileString = Source.fromFile(filePath).mkString
+
+    val temp = preprocess(fileString)
+
+  }
+
+  def preprocess(fileString: String) = {
+
+
 
   }
 }
