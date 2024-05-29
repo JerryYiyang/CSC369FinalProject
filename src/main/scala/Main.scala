276cb01f1fa9d56c8a157e9ce80066e86562cfe1
lamkin1 <jamesclamkin@gmail.com>
2024-05-29 10:31:13 -0700

    kmeans

--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -7,11 +7,14 @@ import org.apache.log4j.Logger
 import org.apache.log4j.Level
 import scala.collection._
 import scala.collection.immutable.List
+import scala.util.Random
 
 object App {
+
   import scala.io.Source
   import java.io._
-  def main(args: Array[String]):Unit = {
+
+  def main(args: Array[String]): Unit = {
     Logger.getLogger("org").setLevel(Level.OFF)
     Logger.getLogger("akka").setLevel(Level.OFF)
     val conf = new SparkConf().setAppName("WordAnalysis").setMaster("local[4]")
@@ -33,17 +36,17 @@ object App {
   }
 
   def preprocess(fileList: List[String]): List[List[String]] = {
-    val punctuation = List(".",",","!","?",":",";","/",'{','}','[',']','(','"',''')
-    val pw = new PrintWriter(new File("hello.txt" ))
+    val punctuation = List(".", ",", "!", "?", ":", ";", "/", '{', '}', '[', ']', '(', '"', ''')
+    val pw = new PrintWriter(new File("hello.txt"))
     var pastFirst = false
     try {
       var totalList = List[List[String]]()
       var indexList = List[String]()
       for (i <- 0 until fileList.length) {
         val element = fileList(i)
-        if (element.nonEmpty){
-          if((!punctuation.contains(element.takeRight(1)))&& (element.takeRight(1).head != '"' && element.takeRight(1).head != ''') && (element.head >= 'A' && element.head <= 'Z')){
-            if(pastFirst) {
+        if (element.nonEmpty) {
+          if ((!punctuation.contains(element.takeRight(1))) && (element.takeRight(1).head != '"' && element.takeRight(1).head != ''') && (element.head >= 'A' && element.head <= 'Z')) {
+            if (pastFirst) {
               totalList = totalList :+ indexList
               indexList = List()
             }
@@ -54,20 +57,15 @@ object App {
           }
 
           //println(totalList.toString())
-          }
         }
-      for (j <- 0 until totalList.length) {
-        println("hit")
-        println(totalList(j))
       }
+//      for (j <- 0 until totalList.length) {
+//        println("hit")
+//        println(totalList(j))
+//      }
 
       totalList
-
-
-
     }
-
-
   }
 
   // gets top 500 used words
@@ -81,14 +79,70 @@ object App {
   def docTF(article: List[String]): List[(String, Double)] = {
     val counts = article.groupBy(x => x).mapValues(_.size).toList
     val totalWords = article.length.toDouble
-    counts.map{case (word, count) => (word, count / totalWords)}
+    counts.map { case (word, count) => (word, count / totalWords) }
   }
 
   // calculates the tfidf
   def tfidf(docTF: List[(String, Double)], top500: List[(String, Double)]): List[Double] = {
     val idfMap = top500.toMap
-    docTF.map{case (word, tf) =>
+    docTF.map { case (word, tf) =>
       val idf = idfMap.getOrElse(word, 0.0)
-      tf * idf}
+      tf * idf
+    }
+  }
+
+  def dotProduct(v1: List[Double], v2: List[Double]): Double = {
+    if (v1.length != v2.length) {
+      throw new IllegalArgumentException("Vectors must have the same length")
+    }
+    (v1 zip v2).map { case (x, y) => x * y }.sum
+  }
+
+  def magnitude(v: List[Double]): Double = {
+    Math.sqrt(v.map(x => x * x).sum)
+  }
+
+  def getCosineDistance(v1: List[Double], v2: List[Double]): Double={
+    1.0 - (dotProduct(v1, v2) /
+      (magnitude(v1) * magnitude(v2)))
+  }
+
+  def greatestCosineDistance(referenceVector: List[Double], listOfVectors: List[List[Double]]): Double = {
+    listOfVectors.map(vector => getCosineDistance(referenceVector, vector)).max
+  }
+
+  def averageVector(points: List[List[Double]]): List[Double] = {
+    val dimensions = points.head.size
+    val averages = (0 until dimensions).map(dim => points.map(_(dim)).sum / points.size).toList
+    averages
+  }
+
+  def kMeansCluster(vectorList: List[List[Double]]): Unit = {
+    val k = 20
+    var hasClusterChange = true
+    var centroids = Random.shuffle(vectorList).take(k)
+
+    while(hasClusterChange){
+      hasClusterChange = false
+      val assignedClusters: List[Int] = vectorList.zipWithIndex.map { case (point, pointIndex) =>
+        val closestCentroidIndex = centroids.zipWithIndex.minBy { case (centroid, _) => getCosineDistance(point, centroid) }._2
+        val previousCluster = if (assignedClusters.nonEmpty) assignedClusters(pointIndex) else -1 // Default to -1 if no previous assignment
+        if (closestCentroidIndex != previousCluster) {
+          hasClusterChange = true
+        }
+        closestCentroidIndex
+      }
+      for (cluster <- 0 until k) {
+        val assignedPoints = vectorList.filter(point => assignedClusters(vectorList.indexOf(point)) == cluster)
+        if (assignedPoints.nonEmpty) {
+          centroids = centroids.updated(cluster, averageVector(assignedPoints))
+        }
+      }
+
+    }
+
+
+
   }
 }
+
