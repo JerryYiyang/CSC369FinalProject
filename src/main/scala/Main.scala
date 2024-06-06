4eb08cb5b0025455d2c64656dd50e6c06ec9851b
JerryYiyang <jerry.yiyang.huo@gmail.com>
2024-06-06 15:48:56 -0700

    slight update to preprocessing

--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -54,12 +54,12 @@ object App {
       if (element.trim.nonEmpty) {
         val isTitle = element.head.isUpper && element.split("\\s+").length <= 5 && !element.exists(
           ch => ch == '-' || ch == ':' || ch == '>' || ch == '<' || ch == "." || ch == "_" || ch == "(" || ch == ")"
-          || ch == "!" || ch == "?" || ch == "/" || ch == "=" || ch == "," || ch == "^")
+          || ch == "!" || ch == "?" || ch == "/" || ch == "=" || ch == "," || ch == "^"|| ch == "=")
         if (isTitle) {
           if (currentArticle.nonEmpty) {
             totalList = totalList :+ currentArticle.toList
             counter += 1
-            if(counter >= 2500){return totalList}
+            if(counter >= 5000){return totalList}
             currentArticle.clear()
           }
           currentArticle += element.toLowerCase
